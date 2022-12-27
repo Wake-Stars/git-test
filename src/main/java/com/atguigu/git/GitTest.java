8431a27edb45ac9874510f9c0f0b651692240119
Alan <1435582884@qq.com>
2022-12-27 20:48:09 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName: GitTest
+ * @Description:
+ * @Author: Stars
+ * @Date: 2022-12-27     20:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+    }
+}
